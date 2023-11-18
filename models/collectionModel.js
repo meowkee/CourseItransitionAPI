703f3f9f7f37990802ce63f81229d2c6aed72ddd
meowkee <pikun.y@yandex.ru>
2023-11-18 14:44:08 +0300

    add field theme to collection

--- models/collectionModel.js
+++ models/collectionModel.js
@@ -4,6 +4,7 @@ import sequelize from "../db.js";
 const Collection = sequelize.define("collection", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING },
+    theme: { type: DataTypes.STRING },
     description: { type: DataTypes.TEXT },
 });
 
